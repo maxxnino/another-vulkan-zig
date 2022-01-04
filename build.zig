e9cf06dcb8db67925e2a6d8309426e02ceae7947
maxxnino <34153891+maxxnino@users.noreply.github.com>
2022-01-05 01:16:43 +0900

    buffer

--- build.zig
+++ build.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 const glfw = @import("libs/mach-glfw/build.zig");
 const vkgen = @import("libs/vulkan-zig/generator/index.zig");
-const zigvulkan = @import("libs/vulkan-zig/build.zig");
+const Step = std.build.Step;
+const Builder = std.build.Builder;
 
 const glfw_pkg = std.build.Pkg{
     .name = "glfw",
@@ -15,8 +16,83 @@ const resources_pkg = std.build.Pkg{
     .name = "resources",
     .path = .{ .path = "zig-cache/resources.zig" },
 };
+const zalgebra_pkg = std.build.Pkg{
+    .name = "zalgebra",
+    .path = .{ .path = "libs/zalgebra/src/main.zig" },
+};
 
-pub fn build(b: *std.build.Builder) void {
+pub const ResourceGenStep = struct {
+    step: Step,
+    shader_step: *vkgen.ShaderCompileStep,
+    builder: *Builder,
+    package: std.build.Pkg,
+    output_file: std.build.GeneratedFile,
+    resources: std.ArrayList(u8),
+
+    pub fn init(builder: *Builder, out: []const u8) *ResourceGenStep {
+        const self = builder.allocator.create(ResourceGenStep) catch unreachable;
+        const full_out_path = std.fs.path.join(builder.allocator, &[_][]const u8{
+            builder.build_root,
+            builder.cache_root,
+            out,
+        }) catch unreachable;
+
+        self.* = .{
+            .step = Step.init(.custom, "resources", builder.allocator, make),
+            .shader_step = vkgen.ShaderCompileStep.init(builder, &[_][]const u8{ "glslc", "--target-env=vulkan1.1" }, "shaders"),
+            .builder = builder,
+            .package = .{
+                .name = "resources",
+                .path = .{ .generated = &self.output_file },
+                .dependencies = null,
+            },
+            .output_file = .{
+                .step = &self.step,
+                .path = full_out_path,
+            },
+            .resources = std.ArrayList(u8).init(builder.allocator),
+        };
+
+        self.step.dependOn(&self.shader_step.step);
+        return self;
+    }
+
+    fn renderPath(path: []const u8, writer: anytype) void {
+        const separators = &[_]u8{ std.fs.path.sep_windows, std.fs.path.sep_posix };
+        var i: usize = 0;
+        while (std.mem.indexOfAnyPos(u8, path, i, separators)) |j| {
+            writer.writeAll(path[i..j]) catch unreachable;
+            switch (std.fs.path.sep) {
+                std.fs.path.sep_windows => writer.writeAll("\\\\") catch unreachable,
+                std.fs.path.sep_posix => writer.writeByte(std.fs.path.sep_posix) catch unreachable,
+                else => unreachable,
+            }
+
+            i = j + 1;
+        }
+        writer.writeAll(path[i..]) catch unreachable;
+    }
+
+    pub fn addShader(self: *ResourceGenStep, name: []const u8, source: []const u8) void {
+        const shader_out_path = self.shader_step.add(source);
+        var writer = self.resources.writer();
+
+        writer.print("pub const {s} = @embedFile(\"", .{name}) catch unreachable;
+        renderPath(shader_out_path, writer);
+        writer.writeAll("\");\n") catch unreachable;
+    }
+
+    fn make(step: *Step) !void {
+        const self = @fieldParentPtr(ResourceGenStep, "step", step);
+        const cwd = std.fs.cwd();
+
+        const dir = std.fs.path.dirname(self.output_file.path.?).?;
+        try cwd.makePath(dir);
+        try cwd.writeFile(self.output_file.path.?, self.resources.items);
+    }
+};
+
+pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
@@ -41,7 +117,7 @@ pub fn build(b: *std.build.Builder) void {
     // shader resources, to be compiled using glslc
     exe.addPackage(blk: {
         if (compile_shader) {
-            const res = zigvulkan.ResourceGenStep.init(b, "resources.zig");
+            const res = ResourceGenStep.init(b, "resources.zig");
             res.addShader("triangle_vert", "shaders/triangle.vert");
             res.addShader("triangle_frag", "shaders/triangle.frag");
             break :blk res.package;
@@ -53,11 +129,14 @@ pub fn build(b: *std.build.Builder) void {
     // mach-glfw
     glfw.link(b, exe, .{});
     exe.addPackage(glfw_pkg);
+    exe.addPackage(zalgebra_pkg);
 
     // single header file
     exe.linkLibCpp();
     exe.addIncludeDir("libs");
     exe.addCSourceFile("libs/vk_mem_alloc.cpp", &.{"-std=c++14"});
+    exe.addCSourceFile("libs/cgltf.c", &.{"-std=c99"});
+    exe.addCSourceFile("libs/stb_image.c", &.{"-std=c99"});
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
---
+++ src/Buffer.zig
@@ -0,0 +1,87 @@
+const std = @import("std");
+const assert = std.debug.assert;
+const vk = @import("vulkan");
+const vma = @import("vma.zig");
+const GraphicsContext = @import("graphics_context.zig").GraphicsContext;
+
+pub const CreateInfo = struct {
+    size: vk.DeviceSize,
+    buffer_usage: vk.BufferUsageFlags,
+    memory_usage: vma.MemoryUsage,
+    memory_flags: vma.AllocationCreateFlags,
+};
+
+const Buffer = @This();
+buffer: vk.Buffer,
+allocation: vma.Allocation,
+info: vma.AllocationInfo,
+size: vk.DeviceSize,
+
+pub fn init(gc: GraphicsContext, create_info: CreateInfo) !Buffer {
+    var allocation_info: vma.AllocationInfo = undefined;
+    const result = try gc.allocator.createBufferAndGetInfo(
+        .{
+            .flags = .{},
+            .size = create_info.size,
+            .usage = create_info.buffer_usage,
+            .sharing_mode = .exclusive,
+            .queue_family_index_count = 0,
+            .p_queue_family_indices = undefined,
+        },
+        .{
+            .flags = create_info.memory_flags,
+            .usage = create_info.memory_usage,
+        },
+        &allocation_info,
+    );
+
+    assert(allocation_info.pMappedData == null and !create_info.memory_flags.contains(.{ .create_mapped = true }));
+    var buffer: Buffer = undefined;
+    buffer.buffer = result.buffer;
+    buffer.allocation = result.allocation;
+    buffer.info = allocation_info;
+    buffer.size = create_info.size;
+    return buffer;
+}
+
+pub fn deinit(self: Buffer, gc: GraphicsContext) void {
+    gc.allocator.destroyBuffer(self.buffer, self.allocation);
+}
+
+pub fn update(self: Buffer, comptime T: type, gc: GraphicsContext, in_data: []const T) !void {
+    const gpu_mem = if (self.info.pMappedData) |data|
+        @intToPtr([*]T, @ptrToInt(data))
+    else
+        try gc.allocator.mapMemory(self.allocation, T);
+
+    for (in_data) |d, i| {
+        gpu_mem[i] = d;
+    }
+    try self.flush(gc);
+}
+
+fn flush(self: Buffer, gc: GraphicsContext) !void {
+    try gc.allocator.flushAllocation(self.allocation, 0, self.info.size);
+    if (self.info.pMappedData == null) {
+        gc.allocator.unmapMemory(self.allocation);
+    }
+}
+
+pub fn copyToBuffer(src: Buffer, dst: Buffer, gc: GraphicsContext) !void {
+    // TODO: because smallest buffer size is 256 byte.
+    // if data size is < 256, group multiple data to one buffer
+    // std.log.info("src: info size: {}, data size: {}", .{src.info.size, src.size});
+    // std.log.info("dst: info size: {}, data size: {}", .{dst.info.size, dst.size});
+    const cmdbuf = try gc.beginOneTimeCommandBuffer();
+
+    const region = vk.BufferCopy{
+        .src_offset = 0,
+        .dst_offset = 0,
+        .size = src.size,
+    };
+    gc.vkd.cmdCopyBuffer(cmdbuf, src.buffer, dst.buffer, 1, @ptrCast([*]const vk.BufferCopy, &region));
+
+    try gc.endOneTimeCommandBuffer(cmdbuf);
+}
+
+pub fn copyToImage() void {}
